ecad02a5d9a3cf28c0ce7517a74d8585bc9e7d96
Howl <121276877+truong20031456@users.noreply.github.com>
2022-12-26 22:42:05 +0700

    Add files via upload

---
+++ Detelecfunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static assignment_22.Filmlibrary;
+
+namespace assignment_22
+{
+     public class Detelecfunction
+    {
+        public  Detelecfunction(String CinemaID) 
+        {
+            
+            
+                foreach (Cinema cinema in Filmlibrary.getInstance().CinemaList)
+                {
+                    if (CinemaID.Equals(cinema.No))
+                    {
+                        Filmlibrary.getInstance().CinemaList.Remove(cinema);
+                    }
+                }
+            
+        }
+            
+    }
+}
---
+++ Filmlibrary.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_22
+{
+    public class xFilmlibrary
+    {
+       
+            public List<Clients> CustomerList { get; set; }
+            public List<Cinema> CinemaList { get; set; }
+            public List<Book> BookingList { get; set; }
+
+            public static Filmlibrary instance = null;
+
+            private xFilmlibrary()
+            {
+                CustomerList = new List<Clients>();
+                CinemaList = new List<Cinema>();
+                BookingList = new List<Book>();
+            }
+
+            public static Filmlibrary getInstance(Filmlibrary instance)
+            {
+                if (instance == null)
+                {
+                  }
+            return instance;
+            }
+
+            public Cinema Cinemafilm(string hotelNo)
+            {
+                foreach (Cinema cinema in CinemaList)
+                {
+                    if (cinema.No == hotelNo)
+                    {
+                        return cinema;
+                    }
+                }
+                return null;
+            }
+        }
+
+    public class Filmlibrary
+    {
+       
+        
+        public List<Clients> CustomerList { get; set; }
+        public List<Cinema> CinemaList { get; set; }
+        public List<Book> BookingList { get; set; }
+
+        public static Filmlibrary instance = null;
+    
+
+        private Filmlibrary()
+        {
+            CustomerList = new List<Clients>();
+            CinemaList = new List<Cinema>();
+            BookingList = new List<Book>();
+        }
+
+        public static Filmlibrary getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Filmlibrary();
+            }
+            return instance;
+        }
+
+        public Cinema Cinemafilm(string hotelNo)
+        {
+            foreach (Cinema cinema in CinemaList)
+            {
+                if (cinema.No == hotelNo)
+                {
+                    return cinema;
+                }
+            }
+            return null;
+        }
+        }
+    }
+
+
